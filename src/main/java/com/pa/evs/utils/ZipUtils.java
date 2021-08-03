b0f0d2ca58b51acc6e834103cb7d3a77843da23c
henrytangvn <85066571+henrytangvn@users.noreply.github.com>
2021-08-03 10:40:40 +0700

    update

--- src/main/java/com/pa/evs/utils/ZipUtils.java
+++ src/main/java/com/pa/evs/utils/ZipUtils.java
@@ -1,51 +1,47 @@
 package com.pa.evs.utils;
 
 import java.io.File;
-import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
-import java.nio.file.Files;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.util.Enumeration;
 import java.util.zip.ZipEntry;
-import java.util.zip.ZipInputStream;
+import java.util.zip.ZipFile;
+
+import org.apache.tomcat.util.http.fileupload.IOUtils;
 
 public class ZipUtils {
 
 	private static final org.slf4j.Logger LOG = org.slf4j.LoggerFactory.getLogger(ZipUtils.class);
-	
+
 	public static void unzip(String zipFilePath, String destDir) {
 		File dir = new File(destDir);
 		if (!dir.exists())
 			dir.mkdirs();
-		FileInputStream fis;
-		byte[] buffer = new byte[1024];
 		try {
-			fis = new FileInputStream(zipFilePath);
-			ZipInputStream zis = new ZipInputStream(fis);
-			ZipEntry ze = zis.getNextEntry();
-			while (ze != null && !ze.isDirectory()) {
-				
-				String fileName = ze.getName();
-				File newFile = new File(destDir + File.separator + fileName);
-				new File(newFile.getParent()).mkdirs();
-				if (!newFile.exists()) {
-					Files.createFile(newFile.toPath());
+			try (java.util.zip.ZipFile zipFile = new ZipFile(zipFilePath)) {
+				Enumeration<? extends ZipEntry> entries = zipFile.entries();
+				while (entries.hasMoreElements()) {
+					try {
+						ZipEntry entry = entries.nextElement();
+						File entryDestination = new File(destDir + File.separator, entry.getName());
+						if (entry.isDirectory()) {
+							entryDestination.mkdirs();
+						} else {
+							entryDestination.getParentFile().mkdirs();
+							try (InputStream in = zipFile.getInputStream(entry);
+									OutputStream out = new FileOutputStream(entryDestination)) {
+								IOUtils.copy(in, out);
+							}
+						}
+					} catch (Exception e) {/**/}
+					
 				}
-				FileOutputStream fos = new FileOutputStream(newFile);
-				int len;
-				while ((len = zis.read(buffer)) > 0) {
-					fos.write(buffer, 0, len);
-				}
-				fos.close();
-				zis.closeEntry();
-				ze = zis.getNextEntry();
 			}
-			zis.closeEntry();
-			zis.close();
-			fis.close();
+
 		} catch (IOException e) {
 			LOG.error(e.getMessage(), e);
 		}
-
 	}
-
 }
